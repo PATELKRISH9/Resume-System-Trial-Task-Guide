74955806892375586245cb612e5c161fb8970bd1
Krish Patel <142641352+PATELKRISH9@users.noreply.github.com>
2025-10-18 16:57:20 +0530

    Update auth.route.js

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const { signup, login, googleSignIn } = require('../controllers/auth.controller');
+const { signup, login } = require('../controllers/auth.controller');
 const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
+const { googleSignIn } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-// Login route
 router.post('/login', loginValidation, login);
-
-// Signup route (lowercase to match frontend)
 router.post('/signup', signupValidation, signup);
 
-// Google Sign-In route
 router.post('/google-sign-in', googleSignIn);
 
 module.exports = router;
