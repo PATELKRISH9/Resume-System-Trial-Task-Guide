f37ba6627db8f3fa53b5daa8b309242d94eeb657
Krish Patel <142641352+PATELKRISH9@users.noreply.github.com>
2025-10-18 19:49:28 +0530

    Update user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         unique: true,
-        sparse: true
+        sparse: true,
     },
     email: {
         type: String,
